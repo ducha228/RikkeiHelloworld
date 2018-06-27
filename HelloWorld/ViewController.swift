aeac815116ab35eb559faba849672d6617edb36b
hand <hand@rikkeisoft.com>
2018-06-27 18:40:18 +0700

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Ha Nguyen Duc on 6/27/18.
+//  Copyright © 2018 Ha Nguyen Duc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
